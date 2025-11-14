d35d1dd0d6a6035636e9c9de26464d12ed846a0d
ChristopherNicolasSMM <christopher.nicolas.mauricio@gmail.com>
2025-11-14 10:28:04 -0300

    Ajuste erro Db Dinamico

--- src/db/database.py
+++ src/db/database.py
@@ -1,5 +1,5 @@
 import os
-if os.getenv('FLASK_ENV') != 'PROD':
+if os.getenv('FLASK_ENV') == 'DEV':
     from db.dev_database import init_db , db, test_connection 
 else:        
     from db.prd_database import init_db , db, test_connection 
