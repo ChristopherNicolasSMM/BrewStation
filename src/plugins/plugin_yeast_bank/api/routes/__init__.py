b2ec9cdb475a13d66acbbc4b2f0384e915dd9935
ChristopherNicolasSMM <christopher.nicolas.mauricio@gmail.com>
2026-02-27 10:57:35 -0300

    chose: menu ok plugin yeastBank

--- src/plugins/plugin_yeast_bank/api/routes/__init__.py
+++ src/plugins/plugin_yeast_bank/api/routes/__init__.py
@@ -2,6 +2,6 @@
 Rotas API do plugin yeast_bank.
 """
 
-from .yeast_bank_routes import plugin_yeast_bank_api
+from .yeast_bank_routes import yeast_bank_bp
 
-all_blueprints = [plugin_yeast_bank_api]
+all_blueprints = [yeast_bank_bp]
